8e8cdc095c7458645fbb7322600a7973abc279c7
Brandon Crowther <bcrowthe11@gmail.com>
2025-12-04 17:41:01 -0400

    Day 03 Problem 01

---
+++ day04/problem01/soln.js
@@ -0,0 +1,70 @@
+import fs from "fs";
+import readline from "readline";
+import process from "process";
+
+const PAPER = "@";
+const EMPTY = ".";
+const MARKED = "x";
+const maxAdjacent = 3;
+
+class Solution {
+  constructor() {
+    this.map = [];
+    this.finalAnswer = 0;
+  }
+
+  inspect(candidateY, candidateX) {
+    let adjacentCount = 0;
+    if (this.map[candidateY][candidateX] === EMPTY) {
+      return;
+    }
+    for (let y = -1; y < 2; y++) {
+      for (let x = -1; x < 2; x++) {
+        if (x === 0 && y === 0) {
+          // No-op
+        } else if (this.map[candidateY + y][candidateX + x] !== EMPTY) {
+          adjacentCount++;
+        }
+      }
+    }
+    if (adjacentCount <= maxAdjacent) {
+      this.map[candidateY][candidateX] = MARKED;
+      this.finalAnswer++;
+    }
+    // this.map[candidateY][candidateX] = adjacentCount;
+  }
+}
+
+// Boilerplate
+async function solution(path) {
+  const fileStream = fs.createReadStream(path, { encoding: "utf8" });
+  const rl = readline.createInterface({
+    input: fileStream,
+  });
+
+  const solution = new Solution();
+  for await (const line of rl) {
+    let lineAsArray = line.split("");
+    // Add extra padding so I dont have to check boundaries
+    lineAsArray = [EMPTY, ...lineAsArray];
+    lineAsArray.push(EMPTY);
+
+    solution.map.push(lineAsArray);
+  }
+  console.table(solution.map);
+  const boundaryLine = solution.map[0].map(() => EMPTY);
+
+  // Add extra lines at top and bottom so I dont have to check boundaries
+  solution.map = [boundaryLine, ...solution.map];
+  solution.map.push(boundaryLine);
+
+  console.table(solution.map);
+  for (let candidateY = 1; candidateY < solution.map.length - 1; candidateY++) {
+    for (let candidateX = 1; candidateX < solution.map[0].length - 1; candidateX++) {
+      solution.inspect(candidateY, candidateX);
+    }
+  }
+  console.table(solution.map);
+  console.log(solution.finalAnswer);
+}
+solution(process.argv[2]);
